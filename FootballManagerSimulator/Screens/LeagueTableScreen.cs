a5f72a98e5476d52d7ff3c70c516c7672b75b392
REMARKABLE\Jordan.Paulson <jordan.paulson@remarkable.net>
2024-04-10 15:20:32 +0100

    Remaining change to separator

--- FootballManagerSimulator/Screens/LeagueTableScreen.cs
+++ FootballManagerSimulator/Screens/LeagueTableScreen.cs
@@ -1,6 +1,7 @@
 ﻿using FootballManagerSimulator.Enums;
 using FootballManagerSimulator.Interfaces;
 using FootballManagerSimulator.Structures;
+using Microsoft.Extensions.Options;
 
 namespace FootballManagerSimulator.Screens;
 
@@ -8,13 +9,16 @@ public class LeagueTableScreen : BaseScreen
 {
     private readonly IState State;
     private readonly IClubHelper ClubHelper;
+    private readonly Settings Settings;
 
     public LeagueTableScreen(
         IState state, 
-        IClubHelper clubHelper) : base(state)
+        IClubHelper clubHelper,
+        IOptions<Settings> settings) : base(state)
     {
         State = state;
         ClubHelper = clubHelper;
+        Settings = settings.Value;
     }
 
     public override ScreenType Screen => ScreenType.LeagueTable;
@@ -42,13 +46,34 @@ public class LeagueTableScreen : BaseScreen
         var leagueTable = league.GenerateLeagueTable(); 
 
         Console.WriteLine($"League Table\n");
-        Console.WriteLine(string.Format("{0,-8}{1,-20}{2,-9}{3,-9}{4,-9}{5,-9}{6,-9}", "Pos", "Team", "Pld", "Points", "For", "Against", "GD"));
+        Console.WriteLine(string.Format("{0,-8}{1,-25}{2,-7}{3,-7}{4,-7}{5,-7}{6,-7}", "Pos", "Team", "Pld", "Pnts", "F", "A", "GD"));
         for (var i = 0; i < leagueTable.Count(); i++)
         {
             var leagueTablePosition = leagueTable.ElementAt(i);
-            Console.WriteLine(string.Format("{0,-8}{1,-20}{2,-9}{2,-9}{3,-9}{4,-9}{5,-9}", 
+            
+            Console.WriteLine(string.Format("{0,-8}{1,-25}{2,-7}{3,-7}{4,-7}{5,-7}{6,-7}", 
                 i+1, leagueTablePosition.ClubName, leagueTablePosition.Played, leagueTablePosition.Points, leagueTablePosition.GoalsScored, leagueTablePosition.GoalsConceded, leagueTablePosition.GoalDifference));
+            if (ShouldAddSeperator(i + 1, league.Id))
+            {
+                Console.WriteLine("----------------------------------------------------------------");
+            }
         }
+    }
+
+    private bool ShouldAddSeperator(int index, int leagueId)
+    {
+        var leagueTableSettings = Settings.Leagues.First(p => p.Id == leagueId).LeagueTable;
+
+        if (leagueTableSettings.AutomaticPromotionPlaces == index)
+            return true;
+
+        if (leagueTableSettings.PlayoffPlaces + leagueTableSettings.AutomaticPromotionPlaces == index)
+            return true;
+
+        if (leagueTableSettings.Places - leagueTableSettings.RelegationPlaces == index)
+            return true;
+
+        return false;
     }
 
     public override void RenderOptions()
